0940b8610537f8f13657fbed524061418657ec63
MarshSuffers <murkymarshmallow@gmail.com>
2026-07-21 10:57:33 -0600

    finished js
    finished all required js, trying to add a button to reset local storage and its not working well. Need to add styling, will add some js for styling buttons

--- script.js
+++ script.js
@@ -1,19 +1,84 @@
 //Declare Variables
-const taskList = ["test"];
+let taskArray = JSON.parse(localStorage.getItem("taskArray"));
+
 const addTaskBtn = document.getElementById("addTaskBtn");
+const deleteBtn = document.getElementById("deleteBtn");
+const editBtn = document.getElementById("editBtn");
+const saveBtn = document.getElementById("saveEditBtn");
+const resetBtn = document.getElementById("resetBtn");
+
+let saveInd = document.getElementById("saveIndex");
+let taskList = document.getElementById("taskList");
 let task = document.getElementById("inputText");
 
-console.log(taskList);
+//Initial Load
+displayTasks();
 
 //Add Task
 addTaskBtn.addEventListener("click", addTask);
 
 function addTask() {
-  console.log("test");
-  taskList.push(task.value);
-  console.log(taskList);
+  if (task.value == "") {
+    alert("Enter Text");
+  } else {
+    taskArray.push(task.value);
+    console.log(taskArray);
+    updateArray();
+    task.value = "";
+    displayTasks();
+  }
 }
 
-console.log(taskList);
-
 //Display Tasks
+function displayTasks() {
+  if (taskArray.length == 0) {
+    taskList.innerHTML = "<p>No Tasks</p>";
+  } else {
+    console.log(taskArray);
+    taskList.innerHTML = "";
+    for (let i = 0; i < taskArray.length; i++) {
+      taskList.innerHTML += `<li>${taskArray[i]}</li><button onclick="deleteTask(${i})">Delete</button>
+      <button onclick="editTask(${i})">Edit</button>`;
+      updateArray();
+    }
+  }
+  updateArray();
+}
+
+//Delete Tasks
+function deleteTask(ind) {
+  console.log(ind);
+  taskArray.splice(indexedDB, 1);
+  displayTasks();
+  console.log(taskArray);
+  updateArray();
+}
+
+//Edit Tasks
+function editTask(ind) {
+  console.log(ind);
+  saveInd.value = ind;
+  task.value = taskArray[saveInd.value];
+}
+
+//Save Edit
+saveBtn.addEventListener("click", saveTask);
+
+function saveTask() {
+  taskArray[saveInd.value] = task.value;
+  displayTasks();
+  updateArray();
+}
+
+//Update Array
+function updateArray() {
+  localStorage.setItem("taskArray", JSON.stringify(taskArray));
+}
+
+//Reset Array
+resetBtn.addEventListener("click", reset);
+
+function reset() {
+  localStorage.clear;
+  displayTasks();
+}
